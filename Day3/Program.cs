7fd90b829208722f448c16ef61459f2924334de1
Caleb VanDerMaas <calebvandermaasc@gmail.com>
2024-12-07 12:24:29 -0500

    Day 4 Init Commit

---
+++ Day3/Program.cs
@@ -0,0 +1,73 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+string filePath = "/Users/calebvandermaas/Developer/Advent-of-Code-2024/Day3/input.txt";
+string[] lines = File.ReadAllLines(filePath);
+// string line = lines[0];
+int runningTotal = 0;
+
+foreach (string line in lines){
+    for (int charIndex = 0; charIndex < line.Length; charIndex++)
+    {
+        char c = line[charIndex];
+        int num1 = 0;
+        int num2 = 0;
+        if (c == 'm' && charIndex != line.Length )
+        {
+            //check for key1 "mul("
+            string check = line.Substring(charIndex, 4);
+            if (check == "mul(")
+            {
+                //proceed with second check "#-###,"
+                int secondIndex = charIndex + 4;
+                for (int i = secondIndex; i <= charIndex + 7; i++)
+                {
+                    //if not a number or comma, then break
+                    if (!Char.IsNumber(line[i]) && line[i] != ',')
+                    {
+                        charIndex = i;
+                        break;
+                    }
+
+                    //if a comma and at least one number, then grab num1
+                    if (line[i] == ',' && i >= charIndex + 5)
+                    {
+                        num1 = Convert.ToInt32(line.Substring(charIndex + 4, i - (charIndex + 4)));
+                        secondIndex = i;
+                        break;
+                    }
+                }
+
+                if (num1 != 0)
+                {
+                    //proceed with second check num2
+                    int thirdIndex = secondIndex + 1;
+                    for (int x = thirdIndex; x <= thirdIndex + 3; x++)
+                    {
+                        if (!Char.IsNumber(line[x]) && line[x] != ')')
+                        {
+                            charIndex = x;
+                            break;
+                        }
+
+                        if (line[x] == ')' && x >= thirdIndex + 1)
+                        {
+                            num2 = Convert.ToInt32(line.Substring(thirdIndex, x - thirdIndex));
+                            // charIndex = x + 1;
+                        }
+                        
+                        if (num2 != 0)
+                        {
+                            int sum = num1 * num2;
+                            runningTotal += sum;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+       
+    }
+}
+
+Console.WriteLine(runningTotal);
+
---
+++ Day4/Program.cs
@@ -0,0 +1,110 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "/Users/calebvandermaas/Developer/Advent-of-Code-2024/Day4/input.txt";
+        string[] lines = File.ReadAllLines(filePath);
+
+        int numXMAS = 0;
+
+        MatrixOperator matrixOperator = new MatrixOperator();
+        char[,] matrix = matrixOperator.InitMatrix(lines);
+
+        for (int row = 0; row < matrix.GetLength(0); row++)
+        {
+            for (int col = 0; col < matrix.GetLength(1); col++)
+            {
+                char value = matrix[row, col];
+                if (value == 'X')
+                {
+                    numXMAS += matrixOperator.XMASLocator(matrix, row, col);
+                }
+            }
+        }
+
+        Console.WriteLine(numXMAS);
+    }
+}
+
+public class MatrixOperator
+{
+    public char[,] InitMatrix(string[] lines)
+    {
+        int lineLength = lines[0].Length;
+        int numRows = lines.Length;
+        char[,] matrix = new char[numRows, lineLength];
+        //init matrix
+        for (int r=0; r < numRows; r++)
+        {
+            string line = lines[r];
+            for (int l=0; l < lineLength; l++)
+            {
+                char c = line[l];
+                matrix[r, l] = c; 
+            }
+        }
+        return matrix;
+    }
+
+    public int XMASLocator(char[,] matrix, int row, int column)
+    {
+        int numLocations = 0;
+        //search up
+        if (row >= 3 && matrix[row-1, column] == 'M' && matrix[row-2, column] == 'A' && matrix[row-3, column] == 'S')
+        {
+            numLocations++;
+        }
+        //search up-right
+        if (row >= 3 && column <= matrix.GetLength(1) - 4 && matrix[row - 1, column + 1] == 'M' && matrix[row - 2, column + 2] == 'A' &&
+            matrix[row - 3, column + 3] == 'S')
+        {
+            numLocations++;
+        }
+        //search right 
+        if (column <= matrix.GetLength(1) - 4 && matrix[row, column + 1] == 'M' && matrix[row, column + 2] == 'A' &&
+            matrix[row, column + 3] == 'S')
+        {
+            numLocations++;
+        }
+        //search right-down
+        if (column <= matrix.GetLength(1) - 4 && row <= matrix.GetLength(0) - 4 && matrix[row + 1, column + 1] == 'M' && matrix[row + 2, column + 2] == 'A' && matrix[row + 3, column + 3] == 'S')
+        {
+            numLocations++;
+        }
+        //search down
+        if (row <= matrix.GetLength(0) - 4 && matrix[row + 1, column] == 'M' && matrix[row + 2, column] == 'A' &&
+            matrix[row + 3, column] == 'S')
+        {
+            numLocations++;
+        }
+        //search down-left
+        if (row <= matrix.GetLength(0) - 4 && column >= 3 && matrix[row + 1, column - 1] == 'M' &&
+            matrix[row + 2, column - 2] == 'A' &&
+            matrix[row + 3, column - 3] == 'S')
+        {
+            numLocations++;
+        }
+        //search left
+        if (column >= 3 && matrix[row, column - 1] == 'M' &&
+            matrix[row, column - 2] == 'A' &&
+            matrix[row, column - 3] == 'S')
+        {
+            numLocations++;
+        }
+        //search up-left
+        if (column >= 3 && row >= 3 && matrix[row - 1, column - 1] == 'M' &&
+            matrix[row - 2, column - 2] == 'A' &&
+            matrix[row - 3, column - 3] == 'S')
+        {
+            numLocations++;
+        }
+
+        return numLocations;
+    }
+    
+}
+
